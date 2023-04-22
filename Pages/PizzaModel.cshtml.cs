ea7d65111cd4230a376a24a2182b890bf2940c6d
AfonsoJ3 <afonsoj3@wit.edu>
2023-04-22 11:41:22 -0400

    Adding comments to the code

--- Pages/PizzaModel.cshtml.cs
+++ Pages/PizzaModel.cshtml.cs
@@ -6,35 +6,47 @@ using RazorPagesPizza.Services;
 namespace RazorPagesPizza.Pages
 {
     public class PizzaModel : PageModel
-{
-    public List<Pizza> pizzas = new();
-    [BindProperty]
-    public Pizza NewPizza { get; set; } = new();
+    {
+        // Field to hold list of pizzas
+        public List<Pizza> pizzas = new();
 
-    public void OnGet()
-    {
-        pizzas = PizzaService.GetAll();
-    }
-    
-    public string GlutenFreeText(Pizza pizza)
-    {
-        return pizza.IsGlutenFree ? "Gluten Free": "Not Gluten Free";
-    }
+        // Property to bind new pizza data from form
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = new();
 
-    public IActionResult OnPost()
-    {
-        if (!ModelState.IsValid)
+        // Method to run when page loads
+        public void OnGet()
         {
-            return Page();
+            // Get all pizzas from the service and assign to field
+            pizzas = PizzaService.GetAll();
         }
-        PizzaService.Add(NewPizza);
-        return RedirectToAction("Get");
-    }
 
-    public IActionResult OnPostDelete(int id)
-    {
-        PizzaService.Delete(id);
-        return RedirectToAction("Get");
+        // Method to return text indicating if pizza is gluten free
+        public string GlutenFreeText(Pizza pizza)
+        {
+            return pizza.IsGlutenFree ? "Gluten Free" : "Not Gluten Free";
+        }
+
+        // Method to run when form is submitted to add new pizza
+        public IActionResult OnPost()
+        {
+            // Check if model state is invalid (e.g. required fields not filled)
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Add new pizza to service and redirect to "Get" method
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        // Method to run when delete button is clicked
+        public IActionResult OnPostDelete(int id)
+        {
+            // Delete pizza with given id from service and redirect to "Get" method
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
     }
-}
 }
