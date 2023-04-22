abcbac4ba5d5b37bc1a628381176c59f14c68242
AfonsoJ3 <afonsoj3@wit.edu>
2023-04-22 11:28:32 -0400

    modify the code and adding comments

--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -1,44 +1,68 @@
-using RazorPagesPizza.Models;
+using RazorPagesPizza.Models; // import the Pizza model
 
-namespace RazorPagesPizza.Services;
-public static class PizzaService
+namespace RazorPagesPizza.Services
 {
-    static List<Pizza> Pizzas { get; }
-    static int nextId = 3;
-    static PizzaService()
+    public static class PizzaService // define the PizzaService class
     {
-        Pizzas = new List<Pizza>
+        // initialize a list of pizzas and a counter for the next ID
+        static List<Pizza> Pizzas { get; }
+        static int nextId = 3;
+
+        // initialize the list of pizzas with some initial values
+        static PizzaService()
+        {
+            Pizzas = new List<Pizza>
+            {
+                new Pizza { Id = 1, Name = "Classic Italian", Price = 20.00M, Size = PizzaSize.Large, IsGlutenFree = false },
+                new Pizza { Id = 2, Name = "Veggie", Price = 15.00M, Size = PizzaSize.Small, IsGlutenFree = true }
+            };
+        }
+
+        // method to get all pizzas in the list
+        public static List<Pizza> GetAll()
+        {
+            return Pizzas;
+        }
+
+        // method to get a pizza by its ID
+        public static Pizza? Get(int id)
+        {
+            foreach (var pizza in Pizzas)
+            {
+                if (pizza.Id == id)
                 {
-                    new Pizza { Id = 1, Name = "Classic Italian", Price=20.00M, Size=PizzaSize.Large, IsGlutenFree = false },
-                    new Pizza { Id = 2, Name = "Veggie", Price=15.00M, Size=PizzaSize.Small, IsGlutenFree = true }
-                };
-    }
+                    return pizza;
+                }
+            }
 
-    public static List<Pizza> GetAll() => Pizzas;
+            return null; // return null if no pizza with the given ID is found
+        }
 
-    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+        // method to add a new pizza to the list
+        public static void Add(Pizza pizza)
+        {
+            pizza.Id = nextId++; // assign a unique ID to the new pizza
+            Pizzas.Add(pizza); // add the new pizza to the list
+        }
 
-    public static void Add(Pizza pizza)
-    {
-        pizza.Id = nextId++;
-        Pizzas.Add(pizza);
-    }
+        // method to delete a pizza from the list by its ID
+        public static void Delete(int id)
+        {
+            var pizza = Get(id); // get the pizza with the given ID
+            if (pizza is null) // if no pizza is found, return early
+                return;
 
-    public static void Delete(int id)
-    {
-        var pizza = Get(id);
-        if (pizza is null)
-            return;
+            Pizzas.Remove(pizza); // remove the pizza from the list
+        }
 
-        Pizzas.Remove(pizza);
-    }
+        // method to update an existing pizza in the list
+        public static void Update(Pizza pizza)
+        {
+            var index = Pizzas.FindIndex(p => p.Id == pizza.Id); // find the index of the pizza with the same ID
+            if (index == -1) // if no pizza is found, return early
+                return;
 
-    public static void Update(Pizza pizza)
-    {
-        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
-        if (index == -1)
-            return;
-
-        Pizzas[index] = pizza;
+            Pizzas[index] = pizza; // update the pizza at the found index
+        }
     }
 }
