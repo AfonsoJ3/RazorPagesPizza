d7563c80c59ef596b3439d92f2f47306751e865c
AfonsoJ3 <afonsoj3@wit.edu>
2023-04-22 10:23:06 -0400

    Add comments to document the code

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -4,15 +4,28 @@ namespace RazorPagesPizza.Models;
 
 public class Pizza
 {
+    // Declare a public integer property called "Id", which represents the unique identifier of a pizza.
     public int Id { get; set; }
 
+    // Add the [Required] attribute to the following property.
     [Required]
+    // Declare a public nullable string '?' property called "Name", which represents the name of the pizza.
     public string? Name { get; set; }
+
+    // Declare a public PizzaSize property called "Size", which represents the size of the pizza.
     public PizzaSize Size { get; set; }
+
+    // Declare a public boolean property called "IsGlutenFree", which represents whether the pizza is gluten-free.
     public bool IsGlutenFree { get; set; }
 
+    // Add the [Range] attribute to the following property.
     [Range(0.01, 9999.99)]
-    public decimal Price { get; set; }
+    // Declare a public decimal property called "Price", which represents the price of the pizza.
+    public decimal Price { get; set; } 
 }
 
-public enum PizzaSize { Small, Medium, Large }
+
+public enum PizzaSize {
+     Small, 
+     Medium, 
+     Large }
